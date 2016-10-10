cace68acf47e820b3be33543d0744613b06f289e
Peli de Halleux <jhalleux@microsoft.com>
2016-10-10 11:20:25 -0700

    updated hello sample

--- libs/hello/hello.ts
+++ libs/hello/hello.ts
@@ -1,7 +1,6 @@
-basic.plotLeds(`
-# # . # #
-. . # . .
-. . # . .
-# . . . #
-. # # # .
-`);
+led.plot(0, 0)
+basic.pause(1000);
+basic.forever(() => {
+    const a = input.acceleration(Dimension.X);
+    led.plotBarGraph(a, 1023)
+})
