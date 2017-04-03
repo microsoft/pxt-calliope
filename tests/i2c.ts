4d9dbb8a6602d77668e88ecb0c488ee407266e51
Matthias L. Jugel <leo@thinkberg.com>
2017-04-03 18:44:25 +0200

    surface repeated start functionality in i2c
    (cherry picked from commit 213c52d)

---
+++ tests/i2c.ts
@@ -0,0 +1,4 @@
+let item = pins.i2cReadNumber(123, NumberFormat.Int8LE)
+pins.i2cWriteNumber(123, 0, NumberFormat.Int8LE)
+let item = pins.i2cReadNumber(123, NumberFormat.Int8LE, true)
+pins.i2cWriteNumber(123, 0, NumberFormat.Int8LE, true)
