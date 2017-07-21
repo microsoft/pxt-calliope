e68e5265285a0433a8f5f6be6d66be26178772c8
Galen Nickel <v-ganick@microsoft.com>
2017-07-21 05:27:28 -0700

    Add help path to jsdoc for pins methods. (#457)
    * Incorrect help link. Fixes pxt#2482.
    
    * Add help path for setPulse. Fixes pxt#1698.

--- libs/core/pins.cpp
+++ libs/core/pins.cpp
@@ -228,6 +228,7 @@ namespace pins {
     */
     //% blockId="pins_pulse_in" block="pulse in (µs)|pin %name|pulsed %value"
     //% weight=20 advanced=true
+    //% help=pins/pulse-in
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     //% name.fieldOptions.tooltips="false" name.fieldOptions.width="300"
     int pulseIn(DigitalPin name, PulseValue value, int maxDuration = 2000000) {
@@ -287,7 +288,7 @@ namespace pins {
      * @param name pin to modulate pitch from
      */
     //% blockId=device_analog_set_pitch_pin block="analog set pitch pin %name"
-    //% help=pins/analog-set-pitch weight=3 advanced=true
+    //% help=pins/analog-set-pitch-pin weight=3 advanced=true
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     //% name.fieldOptions.tooltips="false"
     void analogSetPitchPin(AnalogPin name) {
