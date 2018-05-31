cabda63108dcb9813bbdeb670434ad3e87eb168e
Sam El-Husseini <16690124+samelhusseini@users.noreply.github.com>
2018-05-31 10:04:51 -0700

    Move showNumber to TS and call showString (#829)

--- libs/core/basic.cpp
+++ libs/core/basic.cpp
@@ -8,26 +8,6 @@
 namespace basic {
 
     /**
-     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
-     * @param interval speed of scroll; eg: 150, 100, 200, -100
-     */
-    //% help=basic/show-number
-    //% weight=96
-    //% blockId=device_show_number block="show|number %number" blockGap=8
-    //% async
-    //% parts="ledmatrix"
-    void showNumber(int value, int interval = 150) {
-      if (interval <= 0)
-        interval = 1;
-      ManagedString t(value);
-      if (value < 0 || value >= 10) {
-        uBit.display.scroll(t, interval);
-      } else {
-        uBit.display.printChar(t.charAt(0), interval * 5);
-      }
-    }
-
-    /**
      * Draws an image on the LED screen.
      * @param leds the pattern of LED to turn on/off
      * @param interval time in milliseconds to pause after drawing
--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -131,17 +131,6 @@ declare interface Image {
 declare namespace basic {
 
     /**
-     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
-     * @param interval speed of scroll; eg: 150, 100, 200, -100
-     */
-    //% help=basic/show-number
-    //% weight=96
-    //% blockId=device_show_number block="show|number %number" blockGap=8
-    //% async
-    //% parts="ledmatrix" interval.defl=150 shim=basic::showNumber
-    function showNumber(value: int32, interval?: int32): void;
-
-    /**
      * Draws an image on the LED screen.
      * @param leds the pattern of LED to turn on/off
      * @param interval time in milliseconds to pause after drawing
