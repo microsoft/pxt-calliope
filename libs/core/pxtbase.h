56252de874f7d6860cbb01d3a88e384fbc435ebe
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-04 14:37:26 -0700

    buffer data vs payload app compat fix (#854)
    * buffer data vs payload app compat fix
    
    * added PXT_MICROBIT_TAGGED_INT

--- libs/core/pxtbase.h
+++ libs/core/pxtbase.h
@@ -7,6 +7,14 @@
 #pragma GCC diagnostic ignored "-Wformat"
 #pragma GCC diagnostic ignored "-Warray-bounds"
 
+// helpful define to handle C++ differences in package
+#define PXT_MICROBIT_TAGGED_INT 1
+
+// cross version compatible way of access data field
+#ifndef PXT_BUFFER_DATA
+#define PXT_BUFFER_DATA(buffer) buffer->data
+#endif
+
 // needed for gcc6; not sure why
 #undef min
 #undef max
