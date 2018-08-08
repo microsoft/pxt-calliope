9aac836ae24f28795b64f876ae0527ad16417416
Richard Knoll <riknoll@users.noreply.github.com>
2018-08-08 14:51:00 -0700

    Fix default value for note block (#1053)

--- libs/core/music.ts
+++ libs/core/music.ts
@@ -210,13 +210,13 @@ namespace music {
 
     /**
      * Gets the frequency of a note.
-     * @param name the note name, eg: Note.C
+     * @param name the note name
      */
     //% weight=50 help=music/note-frequency
-    //% blockId=device_note block="%note"
+    //% blockId=device_note block="%name"
     //% shim=TD_ID color="#FFFFFF" colorSecondary="#FFFFFF"
-    //% note.fieldEditor="note" note.defl="262"
-    //% note.fieldOptions.decompileLiterals=true
+    //% name.fieldEditor="note" name.defl="262"
+    //% name.fieldOptions.decompileLiterals=true
     //% useEnumVal=1
     export function noteFrequency(name: Note): number {
         return name;
