2ca97570b4c2c97809a5f50b95164dad5093449d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-04 15:03:02 -0700

    don't rely on webmidi (#1350)
    * don't rely on webmidi
    
    * updated pxt ref

--- sim/state/midi.ts
+++ sim/state/midi.ts
@@ -1,7 +1,6 @@
 namespace pxsim.control {
     export function __midiSend(data: RefBuffer) {
         const b = board();
-        pxsim.AudioContextManager.sendMidiMessageAsync(data)
-            .done();
+        pxsim.AudioContextManager.sendMidiMessage(data);
     }
 }
