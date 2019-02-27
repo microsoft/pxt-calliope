5389392b4e94c3a52accd2a1f29e5e18f7e5984d
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-27 15:24:25 -0800

    reduce GC heap size when BLE is used (#1884)

--- libs/core/pxtcore.h
+++ libs/core/pxtcore.h
@@ -14,7 +14,11 @@ void debuglog(const char *format, ...);
 #define xmalloc malloc
 #define xfree free
 
+#if CONFIG_ENABLED(MICROBIT_BLE_ENABLED)
+#define GC_BLOCK_SIZE 256
+#else
 #define GC_BLOCK_SIZE 1000
+#endif
 
 #define DMESG NOLOG
 
