80845a32f948e15a7f19c905ae46c2b2bef51b35
Amerlander <Amerlander@users.noreply.github.com>
2020-04-21 04:10:27 +0200

    Updates (#96)
    * Update gesture field for new Blockly renderer (#2658)
    
    * Update gesture field for new Blockly renderer
    
    * Bump pxt-core to 5.36.1
    
    * percent sign for motors block
    
    * shims & strings
    
    Co-authored-by: Amerlander <gitkraken@juriwolf.de>

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -598,7 +598,7 @@ declare namespace motors {
      * Turns on the motor at a certain percent of power. Switches to single motor mode!
      * @param power %percent of power sent to the motor. Negative power goes backward. eg: 50
      */
-    //% blockId=motor_on block="motor on at %percent"
+    //% blockId=motor_on block="motor on at %percent \\%"
     //% parts=dcmotor weight=90 blockGap=8
     //% percent.shadow="speedPicker" shim=motors::motorPower
     function motorPower(power: int32): void;
@@ -613,7 +613,7 @@ declare namespace motors {
     /**
      * Controls two motors attached to the board. Switches to dual-motor mode!
      */
-    //% blockId=block_dual_motor block="motor %motor|at %percent"
+    //% blockId=block_dual_motor block="motor %motor|at %percent \\%"
     //% percent.shadow="speedPicker"
     //% weight=80 shim=motors::dualMotorPower
     function dualMotorPower(motor: Motor, duty_percent: int32): void;
