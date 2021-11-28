1828e6399a5e3d3ec3db1f80cd8fdaf7241c76d1
JW <gitkraken@juriwolf.de>
2021-11-28 03:54:02 +0100

    fix storage functions

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -1322,12 +1322,25 @@ declare namespace storage {
      * @param value value to store
      */
     //% weight=100 blockGap=16
-    //% block="Put into %key a value of %value as number"
-    //% blockId=storage_put_value
+    //% block="Put into %key a value of %value as String"
+    //% blockId=storage_put_value_str
     //%
     //% group="Put"
     //% blockHidden=true value.defl=0 shim=storage::putValue
     function putValue(key: string, value?: string): void;
+
+    /**
+     * Saves a key value pair in the non volatile storage
+     * @param key the key for accesing the value
+     * @param value value to store
+     */
+    //% weight=100 blockGap=16
+    //% block="Put into %key a value of %value as Int"
+    //% blockId=storage_put_value_int
+    //%
+    //% group="Put"
+    //% blockHidden=true value.defl=0 shim=storage::putValueInt
+    function putValueInt(key: string, value?: int32): void;
 
     /**
      * Reads a key value pair from the non volatile storage
@@ -1339,6 +1352,17 @@ declare namespace storage {
     //% group="Get"
     //% blockHidden=true shim=storage::getValue
     function getValue(key: string): string;
+
+    /**
+     * Reads a key value pair from the non volatile storage
+     * @param key the key for accesing the value
+     */
+    //% weight=100 blockGap=16
+    //% block="get number from %key"
+    //% blockId=storage_get_value_int
+    //% group="Get"
+    //% blockHidden=true shim=storage::getValueInt
+    function getValueInt(key: string): int32;
 
     /**
      * Reads a key value pair from the non volatile storage
