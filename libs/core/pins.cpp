447df2858c9babdf45d8d1e1fa6fc366906d0567
Amerlander <Amerlander@users.noreply.github.com>
2023-05-04 13:13:07 +0200

    add pins to digital & analog

--- libs/core/pins.cpp
+++ libs/core/pins.cpp
@@ -24,6 +24,9 @@ enum class DigitalPin {
     C16 = MICROBIT_ID_IO_P2,   // RX
     C17 = MICROBIT_ID_IO_P8,   // TX
     C18 = MICROBIT_ID_IO_P20  // SDA
+    A1_RX = MICROBIT_ID_IO_P2,   // RX // C16
+    A1_TX = MICROBIT_ID_IO_P8,   // TX // C17
+    A0_SDA = MICROBIT_ID_IO_P20  // SDA // C18
 };
 
 enum class AnalogPin {
@@ -35,14 +38,16 @@ enum class AnalogPin {
     C16 = MICROBIT_ID_IO_P2,  // RX
     C17 = MICROBIT_ID_IO_P8,  // TX
     MIC = MICROBIT_ID_IO_P21  // microphone
-};
-
-enum class GrovePin {
     A1_RX = MICROBIT_ID_IO_P2,   // RX // C16
     A1_TX = MICROBIT_ID_IO_P8,   // TX // C17
-    A0_SCL = MICROBIT_ID_IO_P19 // SCL // C19
-    A0_SDA = MICROBIT_ID_IO_P20  // SDA // C18
 };
+
+// enum class GrovePin {
+//     A1_RX = MICROBIT_ID_IO_P2,   // RX // C16
+//     A1_TX = MICROBIT_ID_IO_P8,   // TX // C17
+//     A0_SCL = MICROBIT_ID_IO_P19 // SCL // C19
+//     A0_SDA = MICROBIT_ID_IO_P20  // SDA // C18
+// };
 
 enum class PulseValue {
     //% block=high
