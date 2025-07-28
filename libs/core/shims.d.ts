b790bf0896b0893d2432aeb79fae6a89e9f74183
Juri Wolf <Amerlander@users.noreply.github.com>
2025-07-28 15:35:19 +0200

    Refactor microphone handling: move sound level functionality to new microphone library and clean up unused code

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -701,22 +701,6 @@ declare namespace led {
     //% parts="ledmatrix" shim=led::screenshot
     function screenshot(): Image;
 }
-
-
-
-    //% color=#B4009E weight=99 icon="\uf192"
-declare namespace input {
-
-    /**
-     * Reads the loudness through the microphone from 0 (silent) to 255 (loud)
-     */
-    //% help=input/sound-level
-    //% blockId=soundLevel block="sound level"
-    //% parts="microphone"
-    //% weight=34 blockGap=8
-    //% group="Sound" shim=input::soundLevel
-    function soundLevel(): int32;
-}
 declare namespace music {
 
     /**
--- libs/microphone/microphone.cpp
+++ libs/microphone/microphone.cpp
@@ -52,21 +52,38 @@ void onSound(DetectedSound sound, Action handler) {
 #endif
 }
 
+
 /**
 * Reads the loudness through the microphone from 0 (silent) to 255 (loud)
 */
 //% help=input/sound-level
-//% blockId=device_get_sound_level block="sound level"
+//% blockId=soundLevel block="sound level"
 //% parts="microphone"
 //% weight=34 blockGap=8
-//% group="micro:bit (V2)"
+//% group="Sound"
 int soundLevel() {
 #if MICROBIT_CODAL
     init();
     return uBit.audio.levelSPL->getValue();
 #else
-    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
-    return 0;
+    int min = 1023;
+    int max = 0;
+
+    for (int i = 0; i < 32; i++) {
+        int level = uBit.io.MICROPHONE.getAnalogValue();
+        if (level > max) {
+            max = level;
+        }
+        if (level < min) {
+            min = level;
+        }
+        uBit.sleep(5); // Add a small delay to allow the analog input to settle
+    }
+
+    int range = max - min + 0.5;
+    int level = floor(range / 4); // Divide by 4 to get a value between 0 and 255
+
+    return level;
 #endif
 }
 
