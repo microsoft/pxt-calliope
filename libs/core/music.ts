d99c6d18a8901393c4809ae1786b18a562a4d06d
Juri Wolf <Amerlander@users.noreply.github.com>
2025-11-28 18:46:18 +0100

    Update beta reference to v8.1.9

--- libs/core/music.ts
+++ libs/core/music.ts
@@ -199,6 +199,7 @@ namespace music {
     //% parts="speaker"
     //% useEnumVal=1
     //% group="Tone"
+    //% deprecated=1
     export function playTone(frequency: number, ms: number): void {
         if (isNaN(frequency) || isNaN(ms)) return;
         if (_playTone) _playTone(frequency, ms);
@@ -242,7 +243,6 @@ namespace music {
     //% useEnumVal=1
     //% group="Tone"
     //% blockGap=8
-    //% blockHidden=true
     export function noteFrequency(name: Note): number {
         return name;
     }
@@ -326,11 +326,34 @@ namespace music {
     //% blockId=device_builtin_melody block="%melody"
     //% blockHidden=true
     //% group="Melody Advanced"
-    //% advanced=true
     //% deprecated=1
     export function builtInMelody(melody: Melodies): string[] {
         return getMelody(melody);
     }
+
+    // /**
+    //  * Gets the melody array of a built-in melody.
+    //  * @param melody the melody name
+    //  */
+    // //% weight=60 help=music/built-in-playable-melody
+    // //% blockId=device_builtin_melody_playable block="melody $melody"
+    // //% toolboxParent=music_playable_play_default_bkg
+    // //% toolboxParentArgument=toPlay
+    // //% duplicateShadowOnDrag
+    // //% group="Melody Advanced"
+    // export function builtInPlayableMelody(melody: Melodies): StringArrayPlayable {
+    //     return new StringArrayPlayable(getMelody(melody), undefined);
+    // }
+
+    // /**
+    //  * Registers code to run on various melody events
+    //  */
+    // //% blockId=melody_on_event block="music on %value"
+    // //% help=music/on-event weight=59 blockGap=32
+    // //% group="Melody Advanced"
+    // export function onEvent(value: MusicEvent, handler: () => void) {
+    //     control.onEvent(MICROBIT_MELODY_ID, value, handler);
+    // }
 
     /**
      * Use startMelody instead
@@ -338,7 +361,6 @@ namespace music {
     //% hidden=1 deprecated=1
     //% parts="speaker"
     //% group="Melody Advanced"
-    //% advanced=true
     export function beginMelody(melodyArray: string[], options: MelodyOptions = 1) {
         return startMelodyInternal(melodyArray, options);
     }
@@ -353,7 +375,6 @@ namespace music {
     //% blockId=device_start_melody block="start melody %melody=device_builtin_melody| repeating %options"
     //% parts="speaker"
     //% group="Melody Advanced"
-    //% advanced=true
     //% deprecated=1
     export function startMelody(melodyArray: string[], options: MelodyOptions = 1) {
         return startMelodyInternal(melodyArray, options);
@@ -371,6 +392,7 @@ namespace music {
     //% tempo.defl=120
     //% parts="speaker"
     //% group="Melody"
+    //% deprecated=1
     export function playMelody(melody: string, tempo: number) {
         melody = melody || "";
         setTempo(tempo);
@@ -379,7 +401,6 @@ namespace music {
         music.startMelodyInternal(notes, MelodyOptions.Once)
         waitForMelodyEnd();
     }
-
 
     // Shared code between begin, start, and play Melody blocks (all deprecated), plus StringPlayable.play (NOT deprecated).
     export function startMelodyInternal(melodyArray: string[], options: MelodyOptions) {
@@ -482,6 +503,14 @@ namespace music {
             startMelody([], MelodyOptions.Once);
     }
 
+    // MOVED TO CORE MINI CODAL
+    // export function _onStopSound(handler: () => void) {
+    //     if (!stopSoundHandlers) {
+    //         stopSoundHandlers = [];
+    //     }
+    //     stopSoundHandlers.push(handler);
+    // }
+
     /**
      * Stop all sounds and melodies currently playing.
      */
@@ -501,17 +530,17 @@ namespace music {
         }
     }
 
+
     /**
      * Sets a custom playTone function for playing melodies
      */
     //% help=music/set-play-tone
-    //% advanced=true
     //% group="Tone"
     export function setPlayTone(f: (frequency: number, duration: number) => void) {
         _playTone = f;
     }
 
-   /**
+    /**
      * Converts an octave and note offset into an integer frequency.
      * Returns 0 if the note is out of range.
      *
@@ -519,149 +548,145 @@ namespace music {
      * @param note      The offset of the note within the octave
      * @returns         A frequency in HZ or 0 if out of range
      */
-   export function getFrequencyForNote(octave: number, note: number) {
-    return freqs.getNumber(NumberFormat.UInt16LE, (note + (12 * (octave - 1))) * 2) || 0;
-}
-
-/*
- * Converts a simple positive string to an integer.
- * Pxt-common's parseInt is more robust, but it has a fairly large code size footprint.
- * When we know the provided string will be a simple number, we can use this instead,
- * which takes some shortcuts but does not use nearly as much space.
- */
-function parseIntSimple(text: string) {
-    let result = 0;
-    for (let i = 0; i < text.length; ++i) {
-        const c = text.charCodeAt(i) - 48;
-        if (c < 0 || c > 9) return NaN;
-        result = result * 10 + c;
+    export function getFrequencyForNote(octave: number, note: number) {
+        return freqs.getNumber(NumberFormat.UInt16LE, (note + (12 * (octave - 1))) * 2) || 0;
     }
-    return result;
-}
 
-function playNextNote(melody: Melody): void {
-    // cache elements
-    let currNote = melody.nextNote();
-    let currentPos = melody.currentPos;
-    let currentDuration = melody.currentDuration;
-    let currentOctave = melody.currentOctave;
-
-    let note: number;
-    let isrest: boolean = false;
-    let beatPos: number;
-    let parsingOctave: boolean = true;
-    let prevNote: boolean = false;
-
-    for (let pos = 0; pos < currNote.length; pos++) {
-        let noteChar = currNote.charAt(pos);
-        switch (noteChar) {
-            case 'c': case 'C': note = 1; prevNote = true; break;
-            case 'd': case 'D': note = 3; prevNote = true; break;
-            case 'e': case 'E': note = 5; prevNote = true; break;
-            case 'f': case 'F': note = 6; prevNote = true; break;
-            case 'g': case 'G': note = 8; prevNote = true; break;
-            case 'a': case 'A': note = 10; prevNote = true; break;
-            case 'B': note = 12; prevNote = true; break;
-            case 'r': case 'R': isrest = true; prevNote = false; break;
-            case '#': note++; prevNote = false; break;
-            case 'b': if (prevNote) note--; else { note = 12; prevNote = true; } break;
-            case ':': parsingOctave = false; beatPos = pos; prevNote = false; break;
-            default: prevNote = false; if (parsingOctave) currentOctave = parseIntSimple(noteChar);
+    /*
+     * Converts a simple positive string to an integer.
+     * Pxt-common's parseInt is more robust, but it has a fairly large code size footprint.
+     * When we know the provided string will be a simple number, we can use this instead,
+     * which takes some shortcuts but does not use nearly as much space.
+     */
+    function parseIntSimple(text: string) {
+        let result = 0;
+        for (let i = 0; i < text.length; ++i) {
+            const c = text.charCodeAt(i) - 48;
+            if (c < 0 || c > 9) return NaN;
+            result = result * 10 + c;
         }
-    }
-    if (!parsingOctave) {
-        currentDuration = parseIntSimple(currNote.substr(beatPos + 1, currNote.length - beatPos));
-    }
-    let beat = Math.idiv(60000, beatsPerMinute) >> 2;
-    if (isrest) {
-        music.rest(currentDuration * beat)
-    } else {
-        music.playTone(getFrequencyForNote(currentOctave, note), currentDuration * beat);
-    }
-    melody.currentDuration = currentDuration;
-    melody.currentOctave = currentOctave;
-    const repeating = melody.repeating && currentPos == melody.melodyArray.length - 1;
-    melody.currentPos = repeating ? 0 : currentPos + 1;
-
-    control.raiseEvent(MICROBIT_MELODY_ID, melody.background | MusicEvent.MelodyNotePlayed);
-    if (repeating)
-        control.raiseEvent(MICROBIT_MELODY_ID, melody.background | MusicEvent.MelodyRepeated);
-}
-
-
-class Melody {
-    public melodyArray: string[];
-    public currentDuration: number;
-    public currentOctave: number;
-    public currentPos: number;
-    public repeating: boolean;
-
-    // This is bitwise or'd with the events. 0 is not in background, 0xf0 if in background
-    public background: number;
-
-    constructor(melodyArray: string[], options: MelodyOptions) {
-        this.melodyArray = melodyArray;
-        this.repeating = !!(options & (MelodyOptions.Forever | MelodyOptions.ForeverInBackground));
-        this.background = (options & (MelodyOptions.OnceInBackground | MelodyOptions.ForeverInBackground)) ? 0xf0 : 0;
-        this.currentDuration = 4; //Default duration (Crotchet)
-        this.currentOctave = 4; //Middle octave
-        this.currentPos = 0;
+        return result;
     }
 
-    hasNextNote() {
-        return this.repeating || this.currentPos < this.melodyArray.length;
-    }
+    function playNextNote(melody: Melody): void {
+        // cache elements
+        let currNote = melody.nextNote();
+        let currentPos = melody.currentPos;
+        let currentDuration = melody.currentDuration;
+        let currentOctave = melody.currentOctave;
 
-    nextNote(): string {
-        const currentNote = this.melodyArray[this.currentPos];
-        return currentNote;
-    }
-}
+        let note: number;
+        let isrest: boolean = false;
+        let beatPos: number;
+        let parsingOctave: boolean = true;
+        let prevNote: boolean = false;
 
-
-export function _bufferToMelody(melody: Buffer) {
-    if (!melody) return [];
-
-    let currentDuration = 4;
-    let currentOctave = -1;
-    const out: string[] = [];
-
-    const notes = "c#d#ef#g#a#b"
-    let current = "";
-
-    // The buffer format is 2 bytes per note. First note byte is midi
-    // note number, second byte is duration in quarter beats. The note
-    // number 0 is reserved for rests
-    for (let i = 0; i < melody.length; i += 2) {
-        let octave = 4;
-        const note = melody[i] % 12;
-        if (melody[i] === 0) {
-            current = "r"
+        for (let pos = 0; pos < currNote.length; pos++) {
+            let noteChar = currNote.charAt(pos);
+            switch (noteChar) {
+                case 'c': case 'C': note = 1; prevNote = true; break;
+                case 'd': case 'D': note = 3; prevNote = true; break;
+                case 'e': case 'E': note = 5; prevNote = true; break;
+                case 'f': case 'F': note = 6; prevNote = true; break;
+                case 'g': case 'G': note = 8; prevNote = true; break;
+                case 'a': case 'A': note = 10; prevNote = true; break;
+                case 'B': note = 12; prevNote = true; break;
+                case 'r': case 'R': isrest = true; prevNote = false; break;
+                case '#': note++; prevNote = false; break;
+                case 'b': if (prevNote) note--; else { note = 12; prevNote = true; } break;
+                case ':': parsingOctave = false; beatPos = pos; prevNote = false; break;
+                default: prevNote = false; if (parsingOctave) currentOctave = parseIntSimple(noteChar);
+            }
         }
-        else {
-            current = notes.charAt(note);
-            if (current === "#") current = notes.charAt(note - 1) + current
+        if (!parsingOctave) {
+            currentDuration = parseIntSimple(currNote.substr(beatPos + 1, currNote.length - beatPos));
+        }
+        let beat = Math.idiv(60000, beatsPerMinute) >> 2;
+        if (isrest) {
+            music.rest(currentDuration * beat)
+        } else {
+            music.playTone(getFrequencyForNote(currentOctave, note), currentDuration * beat);
+        }
+        melody.currentDuration = currentDuration;
+        melody.currentOctave = currentOctave;
+        const repeating = melody.repeating && currentPos == melody.melodyArray.length - 1;
+        melody.currentPos = repeating ? 0 : currentPos + 1;
 
-            octave = Math.idiv((melody[i] - 24), 12)
+        control.raiseEvent(MICROBIT_MELODY_ID, melody.background | MusicEvent.MelodyNotePlayed);
+        if (repeating)
+            control.raiseEvent(MICROBIT_MELODY_ID, melody.background | MusicEvent.MelodyRepeated);
+    }
+
+    class Melody {
+        public melodyArray: string[];
+        public currentDuration: number;
+        public currentOctave: number;
+        public currentPos: number;
+        public repeating: boolean;
+
+        // This is bitwise or'd with the events. 0 is not in background, 0xf0 if in background
+        public background: number;
+
+        constructor(melodyArray: string[], options: MelodyOptions) {
+            this.melodyArray = melodyArray;
+            this.repeating = !!(options & (MelodyOptions.Forever | MelodyOptions.ForeverInBackground));
+            this.background = (options & (MelodyOptions.OnceInBackground | MelodyOptions.ForeverInBackground)) ? 0xf0 : 0;
+            this.currentDuration = 4; //Default duration (Crotchet)
+            this.currentOctave = 4; //Middle octave
+            this.currentPos = 0;
         }
 
-        const duration = melody[i + 1];
-
-        if (octave !== currentOctave) {
-            current += octave
-            currentOctave = octave;
+        hasNextNote() {
+            return this.repeating || this.currentPos < this.melodyArray.length;
         }
 
-        if (duration !== currentDuration) {
-            current += ":" + duration;
-            currentDuration = duration;
+        nextNote(): string {
+            const currentNote = this.melodyArray[this.currentPos];
+            return currentNote;
         }
-
-        out.push(current);
     }
 
-    return out;
-}
+    export function _bufferToMelody(melody: Buffer) {
+        if (!melody) return [];
 
+        let currentDuration = 4;
+        let currentOctave = -1;
+        const out: string[] = [];
 
+        const notes = "c#d#ef#g#a#b"
+        let current = "";
+
+        // The buffer format is 2 bytes per note. First note byte is midi
+        // note number, second byte is duration in quarter beats. The note
+        // number 0 is reserved for rests
+        for (let i = 0; i < melody.length; i += 2) {
+            let octave = 4;
+            const note = melody[i] % 12;
+            if (melody[i] === 0) {
+                current = "r"
+            }
+            else {
+                current = notes.charAt(note);
+                if (current === "#") current = notes.charAt(note - 1) + current
+
+                octave = Math.idiv((melody[i] - 24), 12)
+            }
+
+            const duration = melody[i + 1];
+
+            if (octave !== currentOctave) {
+                current += octave
+                currentOctave = octave;
+            }
+
+            if (duration !== currentDuration) {
+                current += ":" + duration;
+                currentDuration = duration;
+            }
+
+            out.push(current);
+        }
+
+        return out;
+    }
 }
