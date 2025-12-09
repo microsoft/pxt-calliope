2f62a7045763dab7cd91735d5a1429d17a73e789
Juri Wolf <Amerlander@users.noreply.github.com>
2025-12-09 04:48:55 +0100

    Update 2025 16 (#376)
    * Update beta reference to v8.1.9
    
    * Update codalTarget branch to v0.3.2-calliope-3 with fixed reset pin alias
    
    * refactor music blocks
    
    * Update sound expression groups to categorize under "Sound Effects"
    
    * Revert "Update codalTarget branch to v0.3.2-calliope-3 with fixed reset pin alias"
    
    This reverts commit 56b06236e58c6c8f79ea276768f29666b19ec217.

--- libs/core-mini-codal/music.ts
+++ libs/core-mini-codal/music.ts
@@ -1,36 +1,12 @@
 /**
  * Generation of music tones.
  */
-//% color=#DF4600 weight=98 icon="\uf025"
-//% groups='["Melody", "Tone", "Volume", "Silence", "Tempo", "State", "Melody Advanced"]'
 namespace music {
 
     let stopSoundHandlers: (() => void)[];
     const MICROBIT_MELODY_ID = 2000;
 
-    /**
-     * Gets the melody array of a built-in melody.
-     * @param melody the melody name
-     */
-    //% weight=60 help=music/built-in-playable-melody
-    //% blockId=device_builtin_melody_playable block="melody $melody"
-    //% toolboxParent=music_playable_play_default_bkg
-    //% toolboxParentArgument=toPlay
-    //% duplicateShadowOnDrag
-    //% group="Melody Advanced"
-    export function builtInPlayableMelody(melody: Melodies): StringArrayPlayable {
-        return new StringArrayPlayable(getMelody(melody), undefined);
-    }
 
-    /**
-     * Registers code to run on various melody events
-     */
-    //% blockId=melody_on_event block="music on %value"
-    //% help=music/on-event weight=59 blockGap=32
-    //% group="Melody Advanced"
-    export function onEvent(value: MusicEvent, handler: () => void) {
-        control.onEvent(MICROBIT_MELODY_ID, value, handler);
-    }
 
 
     export function _onStopSound(handler: () => void) {
